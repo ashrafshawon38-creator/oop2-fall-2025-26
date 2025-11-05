4c784f72a360ed87d0bc169d1533813169b6689b
ashrafshawon38-creator <ashrafshawon38@gmail.com>
2025-11-05 16:55:03 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+
+{
+    double price = 250;
+    double finalPrice = price - (price * 0.15);
+    Console.WeiteLine($"This is the final price : {finalPrice Price}");
+    Console.WriteLine($"This is the final price : {finalPrice}");
+}

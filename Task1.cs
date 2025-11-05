1d5613b771800e23b7d15837da785252b923ac66
ashrafshawon38-creator <ashrafshawon38@gmail.com>
2025-11-05 18:31:46 +0600

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,5 @@
+double num3 = Convert.ToDouble(Console.Readline());
+
+double average = (num1 + num2 + num3) / 3.0;
+Console.WriteLine($"Average Score: {average}");
+Console.WriteLine($"Average Score: {average:F2}");

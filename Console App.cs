5fa2a868ed89cb4b416d86257ea26c0b08ea6da9
ashrafshawon38-creator <ashrafshawon38@gmail.com>
2025-11-17 20:59:47 +0600

    Add files via upload

---
+++ Console App.cs
@@ -0,0 +1,24 @@
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.14.36616.10 d17.14
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "ConsoleApp1", "ConsoleApp1\ConsoleApp1.csproj", "{785C96F7-6A13-42A6-84E7-16D74710EFA5}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{785C96F7-6A13-42A6-84E7-16D74710EFA5}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{785C96F7-6A13-42A6-84E7-16D74710EFA5}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{785C96F7-6A13-42A6-84E7-16D74710EFA5}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{785C96F7-6A13-42A6-84E7-16D74710EFA5}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {F44EB362-1C9D-4312-9140-51115537B359}
+	EndGlobalSection
+EndGlobal
